5f0d50e138ff2e921003b08de9eaa49b211065f6
JamieCBarr <jamiecbarr@gmail.com>
2022-06-30 19:25:43 -0400

    added autoclickers

--- DonutMaker.js
+++ DonutMaker.js
@@ -2,10 +2,38 @@ class DonutMaker{
 
     constructor(){
         this.donutCount = 0.0;
+        this.autoClickerCount = 0;
+        this.autoClickerCost = 100;
+        this.autoClickerCostMult = 0.1;
     }
 
     addDonuts(clicks){
         this.donutCount += clicks;
+    }
+
+    getDonutCount(){
+        return Math.floor(this.donutCount);
+    }
+
+    addAutoClickers(numToAdd){
+        this.autoClickerCount += numToAdd;
+    }
+
+    buyAutoClicker(){
+        if (this.isAffordable(this.autoClickerCost)){
+        this.donutCount -= this.autoClickerCost;
+        let costIncrease = Math.floor(this.autoClickerCost * this.autoClickerCostMult);
+        this.autoClickerCost += costIncrease;
+        this.addAutoClickers(1);
+        }
+    }
+
+    activateAutoClickers(){
+        this.addDonuts(this.autoClickerCount);
+    }
+
+    isAffordable(cost){
+        return this.donutCount >= cost;
     }
 }
 
