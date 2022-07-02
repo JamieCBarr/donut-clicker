a4871d3ff526b6f286f7237368672d4084a9cc4b
JamieCBarr <jamiecbarr@gmail.com>
2022-07-02 13:21:11 -0400

    updated index.js -can click to add donuts, and total is displayed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+import DonutMaker from "./DonutMaker";
 
+const donutMaker = new DonutMaker;
+
+renderPage();
+
+function renderPage(){
+  buttonSetup();
+}
+
+function buttonSetup(){
+  const donutBtn = document.querySelector('#donutButton');
+
+  donutBtn.addEventListener('click', ()=>{
+    donutMaker.clickDonuts(1);
+    updateDonutCounter();
+  });
+}
+
+function updateDonutCounter(){
+  const donutCounter = document.querySelector('#donutCounter');
+  donutCounter.innerText = donutMaker.getDonutCount();
+}
